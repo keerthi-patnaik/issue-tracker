f6a90377e3cba8afdb2d278fbedd229219f64210
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-15 16:35:37 -0500

    styling of form and handling errors

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,18 +1,22 @@
 "use client";
 
+import { cn } from "@/lib";
 import { issueSchema } from "@/lib/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
 import axios from "axios";
 import "easymde/dist/easymde.min.css";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, FieldValues, useForm } from "react-hook-form";
+import { IoIosInformationCircleOutline } from "react-icons/io";
 import { SimpleMdeReact } from "react-simplemde-editor";
 import { z } from "zod";
 
 type IssueFormData = z.infer<typeof issueSchema>;
 
 const NewIssuePage = () => {
+  const [error, setError] = useState("");
   const router = useRouter();
   const {
     register,
@@ -21,28 +25,59 @@ const NewIssuePage = () => {
     formState: { errors },
   } = useForm<IssueFormData>({ resolver: zodResolver(issueSchema) });
 
-  const onSubmit = async (body: FieldValues) => {
-    await axios.post("/api/issues", body);
-    router.push("/issues");
+  const onSubmit = async (data: FieldValues) => {
+    try {
+      await axios.post("/api/issues", data);
+      router.push("/issues");
+    } catch (error) {
+      setError("A unexpected error has happened");
+    }
   };
 
   return (
-    <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
-      <TextField.Root>
-        <TextField.Input placeholder="Title" {...register("title")} />
-      </TextField.Root>
-      {errors.title && <p>{errors.title.message}</p>}
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => {
-          return <SimpleMdeReact {...field} placeholder="Description" />;
-        }}
-      />
-      {errors.description && <p>{errors.description.message}</p>}
+    <>
+      {error && (
+        <Callout.Root className="mb-3 max-w-xl text-sm" color="red" size="1">
+          <Callout.Icon>
+            <IoIosInformationCircleOutline />
+          </Callout.Icon>
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
+      <form className="max-w-xl space-y-3" onSubmit={handleSubmit(onSubmit)}>
+        <TextField.Root>
+          <TextField.Input
+            className="focus:border-1 focus:border-cyan-100"
+            placeholder="Title"
+            {...register("title")}
+          />
+        </TextField.Root>
+        {errors.title && (
+          <p className="text-sm text-rose-700">{errors.title.message}</p>
+        )}
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => {
+            return (
+              <SimpleMdeReact
+                {...field}
+                className={cn(
+                  "[&:focus-within_.editor-toolbar]:border-x-1 [&:focus-within_.editor-toolbar]:border-t-1 [&:focus-within_.editor-toolbar]:border-x-cyan-500 [&:focus-within_.editor-toolbar]:border-t-cyan-500",
+                  "[&:focus-within_.CodeMirror-wrap]:border-x-1 [&:focus-within_.CodeMirror-wrap]:border-b-1 [&:focus-within_.CodeMirror-wrap]:border-x-cyan-500 [&:focus-within_.CodeMirror-wrap]:border-b-cyan-500",
+                )}
+                placeholder="Description"
+              />
+            );
+          }}
+        />
+        {errors.description && (
+          <p className="text-sm text-rose-700">{errors.description.message}</p>
+        )}
 
-      <Button type="submit">Submit New Issue</Button>
-    </form>
+        <Button type="submit">Submit New Issue</Button>
+      </form>
+    </>
   );
 };
 
--- lib/schema.ts
+++ lib/schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const issueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
+  title: z.string().min(1, "Please enter the title").max(255),
+  description: z.string().min(1, "Please add the description"),
 });
