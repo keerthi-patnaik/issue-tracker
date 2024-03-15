0156f12daa30a5c89144d3400362c6d6e438d124
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-14 23:08:49 -0500

    Added form for adding new issue

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 const IssuePage = () => {
-  return <div>IssuePage</div>;
+  return (
+    <div>
+      <Link
+        className="h-4 w-4 rounded-md bg-slate-500 px-2 py-2 text-sm font-medium text-slate-100 hover:bg-slate-700"
+        href="/issues/new"
+      >
+        New Issues
+      </Link>
+    </div>
+  );
 };
 
 export default IssuePage;
