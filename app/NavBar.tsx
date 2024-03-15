eef1c5c7c296050c80fe9499a7735471af324dd1
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-14 19:01:01 -0500

    Added navbar stylings

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,33 +1,42 @@
+"use client";
+
+import { cn } from "@/lib";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const NavBarLinks = [
-    {
-      label: "",
-      href: "/",
-    },
     {
       label: "Dashboard",
       href: "/",
     },
     {
       label: "Issues",
-      href: "/",
+      href: "/issues",
     },
   ];
   return (
-    <nav className="mb-5 flex h-14 items-center space-x-6 border-b px-5">
+    <nav className="mb-5 flex h-14 items-center gap-6 border-b px-5">
       <Link href="/">
         <AiFillBug />
       </Link>
-      <ul className="flex space-x-6">
+      <ul className="flex items-center gap-6">
         {NavBarLinks.map((link, index) => {
           return (
             <Link
               key={index}
               href={link.href}
-              className="text-slate-500 transition delay-150 duration-300 hover:text-slate-900 hover:delay-300"
+              className={cn(
+                "transition-color px-4 py-1 text-sm text-slate-500",
+                "hover:text-slate-900",
+                {
+                  "rounded-full bg-slate-100 text-slate-900":
+                    link.href === currentPath,
+                },
+              )}
             >
               {link.label}
             </Link>
