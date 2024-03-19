9ed538dbcd5603771006ec66e98f812ab971f179
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-19 17:14:45 -0500

    improved the layout of navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { cn } from "@/lib";
-import { Box } from "@radix-ui/themes";
+import { Box, Container, Flex } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -22,38 +22,44 @@ const NavBar = () => {
     },
   ];
   return (
-    <nav className="mb-5 flex h-14 items-center gap-6 border-b px-5">
-      <Link href="/">
-        <AiFillBug />
-      </Link>
-      <ul className="flex items-center gap-6">
-        {NavBarLinks.map((link, index) => {
-          return (
-            <li key={index}>
-              <Link
-                href={link.href}
-                className={cn(
-                  "transition-color px-4 py-1 text-sm text-slate-500",
-                  "hover:text-slate-900",
-                  {
-                    "rounded-full bg-slate-100 text-slate-900":
-                      link.href === currentPath,
-                  },
-                )}
-              >
-                {link.label}
-              </Link>
-            </li>
-          );
-        })}
-      </ul>
-      <Box>
-        {status === "unauthenticated" ? (
-          <Link href="/api/auth/signin">LogIn</Link>
-        ) : (
-          <Link href="/api/auth/signout">Log out</Link>
-        )}
-      </Box>
+    <nav className="mb-5 border-b px-5 py-3">
+      <Container>
+        <Flex justify="between">
+          <Flex gap="3" align="center">
+            <Link href="/">
+              <AiFillBug />
+            </Link>
+            <ul className="flex gap-3">
+              {NavBarLinks.map((link, index) => {
+                return (
+                  <li key={index}>
+                    <Link
+                      href={link.href}
+                      className={cn(
+                        "transition-color px-4 py-1 text-sm text-slate-500",
+                        "hover:text-slate-900",
+                        {
+                          "rounded-full bg-slate-100 text-slate-900":
+                            link.href === currentPath,
+                        },
+                      )}
+                    >
+                      {link.label}
+                    </Link>
+                  </li>
+                );
+              })}
+            </ul>
+          </Flex>
+          <Box>
+            {status === "unauthenticated" ? (
+              <Link href="/api/auth/signin">Login</Link>
+            ) : (
+              <Link href="/api/auth/signout">Log out</Link>
+            )}
+          </Box>
+        </Flex>
+      </Container>
     </nav>
   );
 };
