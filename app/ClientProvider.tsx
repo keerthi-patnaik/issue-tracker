b168a4ceae13734e327a3b09640eb13d1bb123ca
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-20 15:03:47 -0500

    Made all provider to one place

--- app/ClientProvider.tsx
+++ app/ClientProvider.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import { PropsWithChildren } from "react";
 
 const queryClient = new QueryClient();
 
 const ClientProvider = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <SessionProvider>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </SessionProvider>
   );
 };
 
