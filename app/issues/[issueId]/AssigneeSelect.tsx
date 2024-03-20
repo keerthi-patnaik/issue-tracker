cd39dde0bfae984b0cbb524257abc89055c9e76a
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-20 15:04:47 -0500

    fetch users

--- app/issues/[issueId]/AssigneeSelect.tsx
+++ app/issues/[issueId]/AssigneeSelect.tsx
@@ -1,14 +1,46 @@
 "use client";
+
+import { Skeleton } from "@/app/components";
+import { User } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 
 const AssigneeSelect = () => {
+  const {
+    isLoading,
+    data: users,
+    error,
+  } = useQuery({
+    queryKey: ["users"],
+    queryFn: async ({ signal }) => {
+      const res = await axios.get<User[]>("/api/users", {
+        signal: signal,
+      });
+
+      return res.data;
+    },
+    staleTime: 60 * 1000, //60s
+    retry: 2,
+  });
+
+  if (isLoading) return <Skeleton height="2rem" />;
+
+  if (error) return null;
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content position="popper">
         <Select.Group>
           <Select.Label>Suggestion</Select.Label>
-          <Select.Item value="1">Keerthi</Select.Item>
+          {users?.map((user) => {
+            return (
+              <Select.Item value={user.id} key={user.id}>
+                {user.name}
+              </Select.Item>
+            );
+          })}
         </Select.Group>
       </Select.Content>
     </Select.Root>
