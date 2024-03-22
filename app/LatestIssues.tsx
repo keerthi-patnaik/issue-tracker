7746bca5f7cff16fc41d0a37879444dbadf6f073
keerthi-patnaik <keerthipatnaik.19@gmail.com>
2024-03-22 16:49:58 -0500

    Build latest issue component

---
+++ app/LatestIssues.tsx
@@ -0,0 +1,52 @@
+import prisma from "@/prisma/client";
+import { Avatar, Card, Flex, Heading, Table } from "@radix-ui/themes";
+import Link from "next/link";
+import { RxAvatar } from "react-icons/rx";
+import { IssueStatusBadge } from "./components";
+
+const LatestIssues = async () => {
+  const latestIssues = await prisma.issue.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+    take: 5,
+    include: {
+      assignedToUser: true,
+    },
+  });
+  return (
+    <Card variant="classic">
+      <Heading size="4" mb="4">
+        Latest Issues
+      </Heading>
+      <Table.Root>
+        <Table.Body className="divide-y">
+          {latestIssues.map((issue) => {
+            return (
+              <Table.Row key={issue.id}>
+                <Table.Cell className="shadow-none">
+                  <Flex justify="between">
+                    <Flex direction="column" align="start" gap="2">
+                      <Link href={`issues/${issue.id}`}>{issue.title}</Link>
+                      <IssueStatusBadge status={issue.status} />
+                    </Flex>
+                    {issue.assignedToUserId && (
+                      <Avatar
+                        size="2"
+                        radius="full"
+                        fallback={<RxAvatar size="25" />}
+                        src={issue.assignedToUser?.image!}
+                      />
+                    )}
+                  </Flex>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table.Root>
+    </Card>
+  );
+};
+
+export default LatestIssues;
